4384a8d7961a54987eb3384ce03ad6eaa1a3b1c1
shingo01060420 <20234117@kcska.onmicrosoft.com>
2026-06-12 00:32:00 +0900

    画像や映像、文章をJSON化して管理
    画像や映像、文章をJSON化して管理

---
+++ UmeArchive/js/chat.js
@@ -0,0 +1,104 @@
+async function loadMessages()
+{
+    const response =
+        await fetch("data/messages.json");
+
+    const messages =
+        await response.json();
+
+    const chatArea =
+        document.getElementById("chatArea");
+
+    messages.forEach(message =>
+    {
+        const wrapper =
+            document.createElement("div");
+
+        wrapper.className = "message";
+
+        let content = "";
+
+        if(message.type === "image")
+        {
+            content =
+            `
+            <img
+                src="${message.image}"
+                class="talk-image"
+                onclick="openImage('${message.image}')">
+
+            <p>
+                ${message.text.replace(/\n/g,"<br>")}
+            </p>
+            `;
+        }
+
+        if(message.type === "text")
+        {
+            content =
+            `
+            <p>
+                ${message.text.replace(/\n/g,"<br>")}
+            </p>
+            `;
+        }
+
+        if(message.type === "video")
+        {
+            content =
+            `
+            <video
+                class="talk-video"
+                controls>
+
+                <source
+                    src="${message.video}"
+                    type="video/mp4">
+
+            </video>
+            `;
+        }
+
+        wrapper.innerHTML =
+        `
+        <div class="message-header">
+
+            <img
+                src="images/ume_icon.jpg"
+                class="mini-icon">
+
+            <span class="member-name">
+                梅澤美波
+            </span>
+
+            <span class="message-date">
+                ${message.date}
+            </span>
+
+        </div>
+
+        <div class="message-box">
+            ${content}
+        </div>
+        `;
+
+        chatArea.appendChild(wrapper);
+    });
+}
+
+function openImage(src)
+{
+    document.getElementById("imageModal")
+        .style.display = "flex";
+
+    document.getElementById("modalImage")
+        .src = src;
+}
+
+function closeImage()
+{
+    document.getElementById("imageModal")
+        .style.display = "none";
+}
+
+loadMessages();
